98465b5b5715840f46a706ea128f3670272948b9
AlemsegedT <lealemseged@gmail.com>
2022-03-16 14:38:10 +0300

    1 and 2

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "1-main.h"
+
+/**
+ * print_alphabet - print all alphabet letters
+ */
+void print_alphabet(void)
+{
+	int m;
+
+	for (m = 'a'; m <= 'z'; m++)
+	{
+		_putchar(m);
+	}
+	_putchar('\n');
+
+}
+
---
+++ 0x02-functions_nested_loops/1-main.h
@@ -0,0 +1,7 @@
+#ifndef _1_MAIN_H_
+#define _1_MAIN_H_
+
+char _putchar(char c);
+void print_alphabet(void);
+#endif /* _1_MAIN_H_*/
+

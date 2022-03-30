7bd3aa28a1ab2cb8c9645364eda001bb05889c04
AlemsegedT <42675517+AlemsegedT@users.noreply.github.com>
2022-03-30 13:30:22 +0300

    0x05 0-9

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+
+#include "main.h"
+
+/**
+* reset_to_98 - sets pointer value of to 98
+* @n: value to reset
+* return: void
+*/
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* puts2 - prints every other character of a string, starting first character
+* @str: pointer
+* Return: 0
+*/
+
+void puts2(char *str)
+{
+	int counter = 0;
+
+	while (*str)
+	{
+	if (counter++ % 2 == 0)
+	_putchar(*str);
+	str++;
+	}
+	_putchar('\n');
+}
